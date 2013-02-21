9f31c1b3e1d830aed2ebe5c43a88b7d2e628a71a
Yngve Bakken Nilsen <yngve.nilsen@gmail.com>
2013-02-21 18:16:24 +0100

    Project creation

---
+++ ContinousDeploymentWithEF/ContinousDeploymentWithEF/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace ContinousDeploymentWithEF.Controllers
+{
+    public class HomeController: Controller // BootstrapBaseController
+    {
+        public ActionResult Index()
+        {
+            List<TestModel> list = new List<TestModel>()
+                {
+                    new TestModel() {Age = 23, Name = "Someone"},
+                    new TestModel() {Age = 30, Name = "Else"}
+                };
+            return View(list);
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+
+        }
+        public ActionResult Details()
+        {
+            return View();
+
+        }
+
+        public ActionResult Delete()
+        {
+            return View();
+
+        }
+
+
+
+    }
+
+    public class TestModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
